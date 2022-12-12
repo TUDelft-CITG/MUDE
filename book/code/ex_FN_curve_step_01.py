11c3f1c05bc8838c1785f50a77cbfe541b635559
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2022-12-12 05:51:10 +0100

    use matplotlibrc

--- book/code/ex_FN_curve_step_01.py
+++ book/code/ex_FN_curve_step_01.py
@@ -2,18 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axisartist.axislines import AxesZero
 
-# TUD Color Palette
-
-def set_TUDstyle():
-    TUcolor = {"cyan": "#00A6D6", "darkgreen": "#009B77", "purple": "#6F1D77",
-               "darkred": "#A50034", "darkblue": "#0C2340",
-               "orange": "#EC6842", "green": "#6CC24A",
-               "lightcyan": "#00B8C8", "red": "#E03C31", "pink": "#EF60A3",
-               "yellow": "#FFB81C", "blue": "#0076C2"}
-    plt.rcParams.update({'axes.prop_cycle': plt.cycler(color=TUcolor.values()),
-                         'font.size': 16, "lines.linewidth": 4})
-    return TUcolor
-
 # Probability density/mass function
 
 fig = plt.figure(figsize=(8,6))
@@ -32,7 +20,6 @@ N_fatalities_loc = [0.05, 0.30, 0.66]
 pdf = ['0.989', '$10^{-2}$', '$10^{-3}$']
 pdf_loc = [0.99, 0.66, 0.33]
 
-set_TUDstyle()
 ax.bar(N_fatalities_loc, pdf_loc, width=0.05, color= 'lightcyan', edgecolor='k')
 
 ax.set_xticks(N_fatalities_loc)
